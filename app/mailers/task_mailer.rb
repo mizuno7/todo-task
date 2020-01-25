ce5455e17e4c6d75fd602e00a6ee7187c43e8ca8
mizuno7 <hiro6031.a@gmail.com>
2020-01-25 11:36:14 +0900

    third commit

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,10 +1,11 @@
 class TaskMailer < ApplicationMailer
-  default from: 'taskleag@example.com'
+  default from: 'taskleaf@example.com'
   def creation_email(task)
     @task = task
     mail(
-      subject: 'タスク完了メール'
-      to: 'mizu6031.a@gmail.com'
+      subject: 'タスク完了メール',
+      to: 'mizu6031.a@gmail.com',
+      from: 'taskleaf@example.com'
     )
   end
 end
